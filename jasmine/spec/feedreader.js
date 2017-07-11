44328fec6d97d4a6b3a5e451d7cdd8125dae9c0a
Eric <ericcstenger@gmail.com>
2017-07-10 19:13:07 -0500

    error expected true to be false in The menu test

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,29 +31,49 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('url defined and not empty', function() {
+         	for (var i = 0; i < allFeeds.length; i++) {
+         		expect(allFeeds[i].url).toBeDefined();
+         		expect(allFeeds[i].url.length).not.toBe(0);
+         	}
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('named defined and not empty', function() {
+         	for (var i = 0; i < allFeeds.length; i++){
+         		expect(allFeeds[i].name).toBeDefined();
+         		expect(allFeeds[i].name.length).not.toBe(0);
+         	}
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function () {
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
-
+     	it('hidden by default', function() {
+     		expect($('body').hasClass('menu-hidden')).toBe(true);
+ 		});
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
-
+        it('changes visibility when clicked', function() {
+        	$('menu-icon-link').click();
+        	expect($('body').hasClass('menu-hidden')).toBe(false);
+        	$('menu-icon-link').click();
+        	expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
+    });	
     /* TODO: Write a new test suite named "Initial Entries" */
 
         /* TODO: Write a test that ensures when the loadFeed
